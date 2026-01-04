7ea8fcd739b729f6f9f510858c356c595860c737
Ralfotar <114840603+ForLess01@users.noreply.github.com>
2026-01-03 21:56:08 -0500

    Agregar servicios de IA: Cerebras, Gemini y Groq; actualizar dependencias y configuración del proyecto

--- index.ts
+++ index.ts
@@ -1,8 +1,45 @@
+import { groqService } from './services/groq';
+import { cerebrasService } from './services/cerebras';
+import type { AIService, ChatMessage } from './types';
+import { geminiService } from './services/gemini';
+
+const services: AIService[] = [
+    groqService,
+    cerebrasService,
+    geminiService,
+];
+
+let currentServiceIndex = 0;
+
+function getNextService() {
+    const service = services[currentServiceIndex];
+    currentServiceIndex = (currentServiceIndex + 1) % services.length;
+    return service;
+}
+
 const server = Bun.serve({
     port: process.env.PORT ?? 3000,
-    async fetch(req) {
-        return new Response("API Bun is running!, correctly!");
-    }
-})
+    async fetch(req: Request) {
+        const { pathname } = new URL(req.url);
 
-console.log(`Server running at ${server.url}:${server.port}`);
+        if (req.method === 'POST' && pathname === '/chat') {
+            const { messages } = await req.json() as { messages: ChatMessage[] };
+            const service = getNextService();
+
+            console.log(`Using service: ${service?.name}`);
+            const stream = await service?.chat(messages);
+
+            return new Response(stream,{
+                headers: {
+                    'Content-Type': 'text/event-stream',
+                    'Cache-Control': 'no-cache',
+                    'Connection': 'keep-alive',
+                },
+            });
+        }
+
+        return new Response("Not found", { status: 404 });
+    },
+});
+
+console.log(`Server is running on ${server.url}`);
